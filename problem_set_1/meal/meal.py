3a40305f32e913d9b0c254670650c20cf2c85bcf
Yair benabou <yairbena@gmail.com>
2024-02-04 15:00:39 +0200

    Added new files

--- problem_set_1/meal/meal.py
+++ problem_set_1/meal/meal.py
@@ -6,26 +6,48 @@
 # And assume that each meal's time range is inclusive.
 # For instance, whether it's 7:00, 7:01, 7:59, or 8:00, or
 # anytime in between, it's time for breakfast.
+# CHALLENGE: If up for a challenge, optionally add support
+# for 12-hour times, allowing the user to input times in
+# these formats too:
+# #:## a.m. and ##:## a.m.
+# #:## p.m. and ##:## p.m.
+
+import re
+import sys
 
 
 def main():
-    print(convert(input("What time is it? ").strip()))
+    time_in_decimal = convert(input("What time is it? ").strip())
+
+    if 7 <= time_in_decimal <= 8:
+        print("breakfast time")
+    elif 12 <= time_in_decimal <= 13:
+        print("lunch time")
+    elif 18 <= time_in_decimal <= 19:
+        print("dinner time")
+    else:
+        print("Not a time to eat!")
 
 
 def convert(time):
-    hour, minutes = map(int, (time.split(":")))
-    minutes_in_decimal = minutes / 60
-    
-    time_in_decimal = hour + minutes_in_decimal
+    twelve_hours_pattern = re.match("^(1[0-2]|0?[0-9])\:[0-5][0-9] ([ap]\.m\.)?$", time, re.IGNORECASE)
+    twenty_four_hours_pattern = re.match("^(2[0-4]|1[0-9]|0?[0-9])\:[0-5][0-9]$", time)
 
-    if 7 <= time_in_decimal <= 8:
-        return "Breakfast time"
-    elif 12 <= time_in_decimal <= 13:
-        return "Lunch time"
-    elif 18 <= time_in_decimal <= 19:
-        return "Dinner time"
+    if not twelve_hours_pattern and not twenty_four_hours_pattern:
+        sys.exit("Invalid input")
+
+    if twenty_four_hours_pattern:
+        hour, minutes = map(int, time.split(":"))
     else:
-        return "Not a time to eat!"
+        hour, minutes_apm = time.split(":")
+        minutes, apm = minutes_apm.split(" ")
+        hour, minutes = map(int, (hour, minutes))
+        if apm.lower() == "p.m.":
+            hour = hour + 12
+
+    time_in_decimal = hour + minutes / 60
+
+    return time_in_decimal
 
 
 if __name__ == "__main__":
